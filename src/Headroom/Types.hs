eff4c68dad6d4445a7d5c11aadcce8f0af693010
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2019-11-13 22:10:20 +0100

    Added support for dynamic part in JSON config

--- src/Headroom/Types.hs
+++ src/Headroom/Types.hs
@@ -12,11 +12,13 @@ import           Data.Aeson                     ( defaultOptions
                                                 , Options
                                                 )
 import qualified Data.Char                     as C
+import qualified Data.Map.Strict               as Map
 import           GHC.Generics                   ( Generic )
 
 data AppConfig =
-    AppConfig { acFoo :: String
-              , acBar :: String
+    AppConfig { acFoo     :: String
+              , acBar     :: String
+              , acOptions :: Map.Map String String
               } deriving (Eq, Generic, Show)
 
 instance FromJSON AppConfig where
--- test/Headroom/ConfigSpec.hs
+++ test/Headroom/ConfigSpec.hs
@@ -3,6 +3,7 @@ module Headroom.ConfigSpec
   )
 where
 
+import qualified Data.Map.Strict               as Map
 import           Test.Hspec
 import           Headroom.Config
 import           Headroom.Types
@@ -12,6 +13,7 @@ spec = do
   describe "parseAppConfig" $ do
     rawAppConfig <- runIO $ loadAppConfig "test-data/test-config.json"
     it "parses configuration from JSON file" $ do
-      let actual   = parseAppConfig rawAppConfig
-          expected = AppConfig "Hello" "World"
+      let options  = Map.fromList [("option1", "value1")]
+          actual   = parseAppConfig rawAppConfig
+          expected = AppConfig "Hello" "World" options
       actual `shouldBe` Just expected
