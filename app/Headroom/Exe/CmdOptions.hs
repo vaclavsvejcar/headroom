4aeddd03692b7d7c9a20fc79badc2992ccb66e8b
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2019-11-23 16:42:07 +0100

    Added more command line arguments

--- app/Headroom/Exe/CmdOptions.hs
+++ app/Headroom/Exe/CmdOptions.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-cse #-}
 {-# OPTIONS_GHC -Wno-partial-fields #-}
-
 module Headroom.Exe.CmdOptions
   ( CmdOptions(..)
   , cmdOptions
@@ -15,21 +15,29 @@ import qualified RIO.Text                      as T
 import           System.Console.CmdArgs
 
 data CmdOptions =
-    Run { foo :: T.Text }
+    Run { source_path :: [FilePath]
+        , template_path :: [FilePath]
+        , replace_headers :: Bool
+        }
   | Generate { bar :: T.Text }
   deriving (Eq, Data, Show, Typeable)
 
 modeRun :: CmdOptions
-modeRun = Run { foo = def &= help "test foo arg" } &= help "run help text"
+modeRun =
+  Run { source_path     = def &= typ "FILE/DIR" &= help "test foo arg"
+      , template_path   = def &= typ "FILE/DIR" &= help "foo"
+      , replace_headers = False &= help "foo bar"
+      }
+    &= help "run help text"
 
-modeTest :: CmdOptions
-modeTest =
+modeGenerate :: CmdOptions
+modeGenerate =
   Generate { bar = def &= help "test bar arg" } &= help "generator help text"
 
 cmdOptions :: Mode (CmdArgs CmdOptions)
 cmdOptions =
   cmdArgsMode
-    $  modes [modeRun, modeTest]
+    $  modes [modeRun, modeGenerate]
     &= help "summary text here"
     &= program "headroom"
     &= summary "headroom v1.0"
