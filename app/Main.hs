51140250ccde31062b4162262e2379423591dcac
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2019-11-25 14:27:34 +0100

    Skeleton for RIO-based app

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,29 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Headroom.Main.CmdOptions
+import           Headroom.Main.App
+import           Headroom.Main.CmdOptions       ( cmdOptions )
 import           RIO
 import           System.Console.CmdArgs
 
 
 main :: IO ()
-main = do
-  opts <- cmdArgsRun cmdOptions
-  runSimpleApp . logInfo . displayShow $ "hello, " ++ show opts
+main = runApp bootstrap
+
+bootstrap :: RIO App ()
+bootstrap = do
+  logInfo "starting app..."
+  displayArgs
+
+runApp :: RIO App a -> IO a
+runApp inner = do
+  cmdOptions' <- cmdArgsRun cmdOptions
+  logOptions  <- logOptionsHandle stderr False
+  withLogFunc logOptions $ \logFunc -> do
+    let app = App { appCmdOptions = cmdOptions', appLogFunc = logFunc }
+    runRIO app inner
+
+displayArgs :: (HasCmdOptions env, HasLogFunc env) => RIO env ()
+displayArgs = do
+  cmdOptions' <- view cmdOptionsL
+  logInfo $ "parsed cmdargs: " <> displayShow cmdOptions'
