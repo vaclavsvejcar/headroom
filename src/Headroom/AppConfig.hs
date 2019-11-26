9a1cde80b1eee09e53d5c67a1720ab6eb3017209
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2019-11-26 15:29:20 +0100

    Replaced concrete monads with more generic type classes

--- src/Headroom/AppConfig.hs
+++ src/Headroom/AppConfig.hs
@@ -5,17 +5,16 @@ module Headroom.AppConfig
   )
 where
 
-import           Control.Arrow                  ( left )
 import qualified Data.Yaml                     as Y
 import           Headroom.Types                 ( AppConfig )
 import           RIO
 import qualified RIO.ByteString                as B
 
 
-loadAppConfig :: FilePath -> IO B.ByteString
-loadAppConfig = B.readFile
+loadAppConfig :: MonadIO m => FilePath -> m AppConfig
+loadAppConfig path = liftIO (B.readFile path >>= parseAppConfig)
 
-parseAppConfig :: B.ByteString -> Either String AppConfig
-parseAppConfig raw = left
-  (\err -> "Cannot parse configuration: " ++ show err)
-  (Y.decodeEither' raw)
+parseAppConfig :: MonadThrow m => B.ByteString -> m AppConfig
+parseAppConfig raw = case Y.decodeEither' raw of
+  Left  err    -> throwM err
+  Right config -> return config
--- test/Headroom/AppConfigSpec.hs
+++ test/Headroom/AppConfigSpec.hs
@@ -16,13 +16,12 @@ spec :: Spec
 spec = do
   describe "parseAppConfig" $ do
     it "parses full configuration from YAML file" $ do
-      rawAppConfig <- loadAppConfig "test-data/configs/full.yaml"
+      appConfig <- loadAppConfig "test-data/configs/full.yaml"
       let options = HM.fromList
             [ ("copyright", "(c) 2019 John Smith")
             , ("email"    , "john.smith@example.com")
             ]
           sourcePaths   = ["path/to/src"]
           templatePaths = ["path/to/dir1", "path/to/dir2"]
-          actual        = parseAppConfig rawAppConfig
           expected      = AppConfig 1 True sourcePaths templatePaths options
-      actual `shouldBe` Right expected
+      appConfig `shouldBe` expected
