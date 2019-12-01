acade6ccf830db9887826f3e0b0a36d9feb6adaf
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2019-12-01 10:38:38 +0100

    [#5] Make paths in AppConfig relative to AppConfig location

--- src/Headroom/AppConfig.hs
+++ src/Headroom/AppConfig.hs
@@ -1,23 +1,36 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Headroom.AppConfig
   ( loadAppConfig
+  , makePathsRelativeTo
   , parseAppConfig
   , parsePlaceholders
   )
 where
 
 import qualified Data.Yaml                     as Y
-import           Headroom.Types                 ( AppConfig
+import           Headroom.Types                 ( AppConfig(..)
                                                 , HeadroomError(..)
                                                 )
 import           RIO
 import qualified RIO.ByteString                as B
+import           RIO.FilePath                   ( (</>)
+                                                , takeDirectory
+                                                )
 import qualified RIO.HashMap                   as HM
 import qualified RIO.Text                      as T
 
 
 loadAppConfig :: MonadIO m => FilePath -> m AppConfig
-loadAppConfig path = liftIO $ B.readFile path >>= parseAppConfig
+loadAppConfig path = do
+  appConfig <- liftIO $ B.readFile path >>= parseAppConfig
+  return $ makePathsRelativeTo (takeDirectory path) appConfig
+
+makePathsRelativeTo :: FilePath -> AppConfig -> AppConfig
+makePathsRelativeTo root appConfig = appConfig
+  { acSourcePaths   = processPaths . acSourcePaths $ appConfig
+  , acTemplatePaths = processPaths . acTemplatePaths $ appConfig
+  }
+  where processPaths = fmap (root </>)
 
 parseAppConfig :: MonadThrow m => B.ByteString -> m AppConfig
 parseAppConfig = Y.decodeThrow
--- test/Headroom/AppConfigSpec.hs
+++ test/Headroom/AppConfigSpec.hs
@@ -14,16 +14,17 @@ import qualified RIO.HashMap                   as HM
 
 spec :: Spec
 spec = do
-  describe "parseAppConfig" $ do
-    it "parses full configuration from YAML file" $ do
+  describe "loadAppConfig" $ do
+    it "loads full configuration from YAML file" $ do
       appConfig <- loadAppConfig "test-data/configs/full.yaml"
       let options = HM.fromList
             [ ("copyright", "(c) 2019 John Smith")
             , ("email"    , "john.smith@example.com")
             ]
-          sourcePaths   = ["path/to/src"]
-          templatePaths = ["path/to/dir1", "path/to/dir2"]
-          expected      = AppConfig 1 True sourcePaths templatePaths options
+          sourcePaths = ["test-data/configs/path/to/src"]
+          templatePaths =
+            ["test-data/configs/path/to/dir1", "test-data/configs/path/to/dir2"]
+          expected = AppConfig 1 True sourcePaths templatePaths options
       appConfig `shouldBe` expected
 
   describe "parsePlaceholders" $ do
