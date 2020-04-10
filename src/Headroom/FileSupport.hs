f1f38b4560765feb00a7ad8f4712822d296a9690
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2020-04-10 13:40:27 +0200

    [#28] Initial support for adding license headers

--- src/Headroom/FileSupport.hs
+++ src/Headroom/FileSupport.hs
@@ -2,9 +2,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 module Headroom.FileSupport
-  ( extractFileInfo
+  ( addHeader
+  , extractFileInfo
   , findHeaderPos
   , lastMatching
+  , splitAtHeader
   )
 where
 
@@ -22,6 +24,12 @@ import           Text.Regex.PCRE.Light          ( Regex
 import           Text.Regex.PCRE.Light.Char8    ( utf8 )
 
 
+addHeader :: FileInfo -> Text -> Text -> Text
+addHeader FileInfo {..} header text = T.intercalate "\n" withHeader
+ where
+  (_, before, after) = splitAtHeader (hcPutAfter fiHeaderConfig) text
+  withHeader         = concat [before, [header], after]
+
 extractFileInfo :: FileType -> HeaderConfig -> Text -> FileInfo
 extractFileInfo fiFileType fiHeaderConfig input =
   let fiHeaderPos = findHeaderPos fiHeaderConfig input
@@ -35,13 +43,10 @@ extractVariables _ _ _ = HM.empty
 findHeaderPos :: HeaderConfig -> Text -> Maybe (Int, Int)
 findHeaderPos HeaderConfig {..} input = position
  where
-  headerArea = drop splitAt inputLines
-  inputLines = T.lines input
-  splitAt    = if null hcPutAfter then 0 else lastMatching regex inputLines
-  regex      = compile (encodeUtf8 $ T.intercalate "|" hcPutAfter) [utf8]
-  isStart    = T.isPrefixOf hcStartsWith
-  isEnd      = T.isSuffixOf hcEndsWith
-  position   = go (T.strip <$> headerArea) Nothing Nothing splitAt
+  (splitAt, _, headerArea) = splitAtHeader hcPutAfter input
+  isStart                  = T.isPrefixOf hcStartsWith
+  isEnd                    = T.isSuffixOf hcEndsWith
+  position                 = go (T.strip <$> headerArea) Nothing Nothing splitAt
    where
     go (x : _) _ _ i | isStart x && isEnd x = Just (i, i)
     go (x : xs) _ _ i | isStart x           = go xs (Just i) Nothing (i + 1)
@@ -55,3 +60,13 @@ lastMatching regex input = go input 0 0
   go (x : xs) _ i | isJust $ match regex (encodeUtf8 x) [] = go xs i (i + 1)
   go (_ : xs) pos i = go xs pos (i + 1)
   go []       pos _ = pos
+
+splitAtHeader :: [Text] -> Text -> (Int, [Text], [Text])
+splitAtHeader []       input = (0, [], T.lines input)
+splitAtHeader putAfter input = (splitAt, before, after)
+ where
+  before  = take splitAt inLines
+  after   = drop splitAt inLines
+  inLines = T.lines input
+  regex   = compile (encodeUtf8 $ T.intercalate "|" putAfter) [utf8]
+  splitAt = lastMatching regex inLines + 1
