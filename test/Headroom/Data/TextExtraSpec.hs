83be26d3e6c62ff8805a58e3836b4579d064f492
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2020-06-07 21:49:26 +0200

    Add missing test

--- test/Headroom/Data/TextExtraSpec.hs
+++ test/Headroom/Data/TextExtraSpec.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
 module Headroom.Data.TextExtraSpec
   ( spec
   )
@@ -23,3 +24,8 @@ spec = do
           fn       = ("L: " <>)
           expected = T.unlines ["L: foo zz", "L: bar", "L: xx"]
       mapLines fn sample `shouldBe` expected
+
+
+  describe "read" $ do
+    it "parses value from given text using Read instance" $ do
+      read @Int "123" `shouldBe` Just 123
