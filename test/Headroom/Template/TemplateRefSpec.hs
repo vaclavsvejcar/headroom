583afa8d4e682fdcaf8f119137b267ebddbbaa8b
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2021-04-08 16:15:08 +0200

    [#66] Simplify TemplateRef

--- test/Headroom/Template/TemplateRefSpec.hs
+++ test/Headroom/Template/TemplateRefSpec.hs
@@ -10,10 +10,9 @@ module Headroom.Template.TemplateRefSpec
 where
 
 
-import           Headroom.FileType.Types             ( FileType(..) )
-import           Headroom.Template.Mustache          ( Mustache )
 import           Headroom.Template.TemplateRef
 import           RIO
+import qualified RIO.List                           as L
 import           Test.Hspec
 import           Text.URI.QQ                         ( uri )
 
@@ -24,23 +23,29 @@ spec = do
   describe "mkTemplateRef" $ do
     it "creates valid reference to local Haskell template" $ do
       let raw      = "/path/to/some/haskell.mustache"
-          expected = TemplateRef
-            { trFileType = Haskell
-            , trSource   = LocalTemplateSource "/path/to/some/haskell.mustache"
-            }
-      mkTemplateRef @Mustache raw `shouldBe` Just expected
+          expected = LocalTemplateRef "/path/to/some/haskell.mustache"
+      mkTemplateRef raw `shouldBe` Just expected
 
     it "creates valid reference to HTTP Haskell template" $ do
       let raw      = "http://foo/haskell.mustache"
-          expected = TemplateRef
-            { trFileType = Haskell
-            , trSource   = UriTemplateSource [uri|http://foo/haskell.mustache|]
-            }
-      mkTemplateRef @Mustache raw `shouldBe` Just expected
+          expected = UriTemplateRef [uri|http://foo/haskell.mustache|]
+      mkTemplateRef raw `shouldBe` Just expected
 
     it "throws error if URI is valid but protocol is not supported" $ do
       let raw = "foo://foo/haskell.mustache"
-      mkTemplateRef @Mustache raw `shouldThrow` \case
+      mkTemplateRef raw `shouldThrow` \case
         (UnsupportedUriProtocol _ _) -> True
         _                            -> False
 
+
+  describe "Ord instance for TemplateRef" $ do
+    it "should properly order records" $ do
+      let sample =
+            [ UriTemplateRef [uri|http://foo/haskell.mustache|]
+            , LocalTemplateRef "/path/to/some/haskell.mustache"
+            ]
+          expected =
+            [ LocalTemplateRef "/path/to/some/haskell.mustache"
+            , UriTemplateRef [uri|http://foo/haskell.mustache|]
+            ]
+      L.sort sample `shouldBe` expected
